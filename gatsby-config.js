3afe04859c184b5d280719711b04254378cb11fd
Lever <wangzheng@shimo.im>
2019-01-17 22:00:54 +0800

    feat: add google analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-132700103-1`,
       },
     },
     `gatsby-plugin-feed`,
